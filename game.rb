2be5bfc372cb5ea885bdbf5aac414b3042ab6d0d
lonewarrior556 <lonewarrior556@yahoo.com>
2015-04-02 14:25:36 -0400

    add gamepay functionality

--- game.rb
+++ game.rb
@@ -19,3 +19,48 @@ class Game
 
   def initialize
     @game_board= Board.new
+    @turn = white
+  end
+
+  def get_move
+    begin
+    puts "please enter move ie. a1,b2"
+    pos = gets.chomp.split
+
+    from, to = pos
+    raise "invalid entry" if !INPUT_TRANSLATION.include?(to) or !INPUT_TRANSLATION.include?(from)
+
+    from = INPUT_TRANSLATION[from]
+    to = INPUT_TRANSLATION[to]
+
+    raise "No piece there" if @game_board.tiles[from].class != Checker
+    raise "Not yo piece" if @game_board.tiles[from].color != turn
+
+  rescue => error
+    puts error
+    retry
+  end
+
+    [from, to]
+  end
+  def play
+    while true
+      puts "#{turn}'s turn'"
+      checker , position = get_move
+    begin
+       @game_board.tiles[checker].move(position) if @game_board.tiles[checker].legal?(position)
+
+    rescue => error
+      puts error
+    end
+
+    end
+
+  end
+
+
+
+
+
+
+end
