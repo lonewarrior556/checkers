7e0d12797bde7b4e76c6dea68599825d23190034
App Academy Student <appacademy@Apps-Mac-mini-8.local>
2015-04-02 10:07:45 -0400

    create and initialize piece class, create board class

---
+++ piece.rb
@@ -0,0 +1,25 @@
+class Piece
+
+  attr_reader :color, :board
+
+  attr_accessor :king, :position
+
+
+    def initialize(color, king, position, board)
+      @color = color
+      @king = false
+      @position = position
+      @board = board
+    end
+
+    
+
+
+
+
+
+
+
+
+
+end
