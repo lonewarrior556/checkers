a1a97ec4d3aa43ed02a20986acaa544fef7def51
lonewarrior556 <lonewarrior556@yahoo.com>
2015-04-02 12:15:59 -0400

    add constants, define move method

--- checker.rb
+++ checker.rb
@@ -1,43 +1,52 @@
 class Checker
 
+  MOTIONS = [8,9,16,18,-8,-9,-16,-18]
+
+  DRAW = { [true, :black]=>♚, [true, :white] =>♔,
+      [false, :black] => ◉, [false, :white] => ◎}
+
+
   attr_reader :color, :board
 
   attr_accessor :king, :position
 
 
-    def initialize(color, value, board)
-      @color = color
-      @king = false
-      @value = value
-      @board = board
-    end
+  def initialize(color, value, board)
+    @color = color
+    @king = false
+    @value = value
+    @board = board
+  end
 
 
-    def legal?(pos)
-      motions =[8,9,16,18,-8,-9,-16,-18]
-      if king
-        "nothing"
-      elsif color == :black
-        motions = motions[4..-1]
-      else
-        motions - motions[0..3]
-      end
-      raise "illegal square" if !Board::LEGAL_POSITIONS.include?(pos)
+  def legal?(pos)
+    motions = MOTIONS[4..-1] if color == :black
+    motions = MOTIONS[0..3]  if color == :white
+    motions = MOTIONS if king
 
-      raise "illegal move" if !motions.map{|x|x + @value}.include?(pos)
+    raise "illegal square" if !Board::LEGAL_POSITIONS.include?(pos)
 
-      raise "piece there" if !@board.tiles[pos].nil?
+    raise "illegal move" if !motions.map{|x|x + @value}.include?(pos)
 
-      raise "no piece to jump" if (pos-value)>10 && !@board.tiles[(pos-value)/2 + value].nil?
+    raise "piece there" if !@board.tiles[pos].nil?
 
-      true
-    end
+    raise "no piece to jump" if (pos-value).abs > 10 && !@board.tiles[(pos-value)/2 + value].nil?
+
+    true
+  end
 
 
+  def move(pos)
+    @board.tiles[(pos-value)/2 + value] = nil if (pos-value).abs > 10
+    @board.tiles[value] = nil
+    @value = pos
+    @board.tiles[value] = self
+  end
 
-    def move(pos)
 
-
+  def show
+    DRAW[king,color]
+  end
 
 
 
